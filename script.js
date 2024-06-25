04b9997800dde85e487013594d50367a161d1762
goot72 <157259472+goot72@users.noreply.github.com>
2024-06-25 13:55:33 -0700

    edit the weather cards

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const createWeatherCard = (cityName, weatherItem, index) => {
     if(index === 0) { // HTML for the main weather card
         return `<div class="details">
                     <h2>${cityName} (${weatherItem.dt_txt.split(" ")[0]})</h2>
-                    <h6>Temperature: ${(weatherItem.main.temp - 273.15). toFixed(2)}°C</h6>
+                    <h6>Temperature: ${((weatherItem.main.temp - 273.15) * 9/5 + 32 ). toFixed(2)}°f</h6>
                     <h6>Wind: ${weatherItem.wind.speed} M/S</h6>
                     <h6>Humidity: ${weatherItem.main.humidity}%</h6>
                 </div>
@@ -22,7 +22,7 @@ const createWeatherCard = (cityName, weatherItem, index) => {
         return `<li class="card">
                     <h3>(${weatherItem.dt_txt.split(" ")[0]})</h3>
                     <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@4x.png" alt="weather-icon">
-                    <h6>Temp: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h6>
+                    <h6>Temp: ${((weatherItem.main.temp - 273.15)* 9/5 + 32).toFixed(2)}°f</h6>
                     <h6>Wind: ${weatherItem.wind.speed} M/S</h6>
                     <h6>Humidity: ${weatherItem.main.humidity}%</h6>
                 </li>`;
